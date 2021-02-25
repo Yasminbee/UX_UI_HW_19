68586ce7b6e9f29fb19502b52881347fb02bd3b2
Yasminbee <yasmin.bakhtiari@gmail.com>
2021-02-24 22:33:52 -0500

    Final Update UI

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
   console.log("Your index.js file is loaded correctly!");
  
-  $('#button2').on("click", function(){
-    $('.cssChange').css("background-color","red");
+  $("#button2").on("click", function(){
+    $('#button2').css("background-color","green");
   });
+  
